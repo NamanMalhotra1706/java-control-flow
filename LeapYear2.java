669a0c3bf15d5d945eb2f579a1693728f2e3da9f
Naman Malhotra <naman0913.be21@chitkara.edu.in>
2025-03-02 22:57:20 +0530

    [Naman Malhotra] ADD: Leap Year using logical and && and or || operators

---
+++ LeapYear2.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class LeapYear2{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int year;
+		
+		while(true){
+			System.out.print("Enter the Year: ");
+			year = sc.nextInt();
+			if(year<1582){
+				System.out.print("Year must me greater than 1582");
+			}
+			else{
+				break;
+			}
+		}
+		if(year%400==0 && year%100==0 && year%4==0){
+			
+			System.out.print("Leap Year");
+		}
+		else{
+			System.out.print("Not a leap Year");
+		}
+	}
+}
