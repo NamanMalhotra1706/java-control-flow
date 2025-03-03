fee4e303457ed6e70998c1d09b58914e9d764fa6
Naman Malhotra <naman0913.be21@chitkara.edu.in>
2025-03-03 13:28:00 +0530

    [Naman Malhotra] MODIFY: Fizz Buzz problem divisible by 3 and 5 both

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -23,6 +23,9 @@ class FizzBuzz{
 			else if(i%5==0){
 				System.out.println("Buzz");
 			}
+			else if(i%5==0 && i%3==0){
+				System.out.println("FizzBuzz");
+			}
 			else{
 				System.out.println(i);
 			}
